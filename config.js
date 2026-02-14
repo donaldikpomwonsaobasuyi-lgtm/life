884d3ae0da8bc81df57cea4e85d5c951a955b5df
donaldikpomwonsaobasuyi-lgtm <donaldikpomwonsa.obasuyi@gmail.com>
2026-02-14 10:40:55 +0100

    Update music URL for streaming

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "Acceptable-music-promscom-Die-with-a-smile-viral-song-mp3-download-free.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dyvq9vdij/video/upload/v1771061926/Dominic_Fike_-_white_keys_-_RockHype_youtube_ig4glv.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
